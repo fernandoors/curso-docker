40827a2e3954b62e8e70fc7816ad13ead0ae4143
fernandoors <fernando.ors@outlook.com>
2018-08-12 20:34:52 -0300

    api

--- ingresse/compose/backend/app.js
+++ ingresse/compose/backend/app.js
@@ -10,16 +10,20 @@ const cors = require('cors')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-//Teste
-server.get('/', (requestAnimationFrame, res, next) => res.send('Backend'))
-
+//Middlewares
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
 server.use(cors())
 
+//ODM
+const Client = restful.model('Client', {
+    name: { type: String, require: true}
+})
+
+//REST
 Client.methods(['get', 'post', 'put', 'delete'])
 Client.updateOptions({new: true, runValidators:true})
-
 Client.register(server, '/clients')
+
 //Start Server
 server.listen(3000)
