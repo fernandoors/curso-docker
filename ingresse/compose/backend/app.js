95bb0445d2907fe85d4d55fee0c9f73744c8ef5e
fernandoors <fernando.ors@outlook.com>
2018-08-12 02:11:17 -0300

    backend

--- ingresse/compose/backend/app.js
+++ ingresse/compose/backend/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 
 //Database
 mongoose.Promise = global.Promise
@@ -10,5 +13,13 @@ mongoose.connect('mongodb://db/mydb')
 //Teste
 server.get('/', (requestAnimationFrame, res, next) => res.send('Backend'))
 
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators:true})
+
+Client.register(server, '/clients')
 //Start Server
 server.listen(3000)
